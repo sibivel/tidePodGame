19ea2c9588163d058daf29beb8d1ec4909abfbf6
Sibi Sengottuvel <sibivel979@gmail.com>
2018-01-20 03:36:33 -0500

    donezo

--- js/game.js
+++ js/game.js
@@ -1,17 +1,28 @@
 
 var myGamePiece;
 var myObstacles = [];//tide pods
+var lives = [];
 var myScore;
+var ended = false;
 var fallSpeed = 5;
 var face = document.createElement('img');
 var pod = document.createElement('img');
+var highscore = -1;
 face.src = "./images/open-mouth.png";
 pod.src = "./images/tide-pods.png";
 function startGame() {
    
     myScore = new component("30px", "Consolas", "black", 280, 40, "text");
-    mySpeed = new component("30px", "Consolas", "black", 40, 40, "text");
+    myHighScore = new component("30px", "Consolas", "black", 5, 80, "text");
+    if(highscore > 0){
+       myHighScore.text = "High Score: " + highscore;
+    }else{
+        myHighScore.text = ""
+    }
     myGameArea.start();
+    for(var i = 0; i < 3; i+=1){
+        lives.push(new component(30, 30, "blue", i*40, 10, "life"))
+    }
     var mult = 6
     width = 11*mult;
     height = 15*mult;
@@ -32,10 +43,29 @@ var myGameArea = {
         },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
+    },
+    again : function() {
+        ended = false;
+        myGameArea.frameNo = 0;
+        fallSpeed = 5;
+        myGameArea.clear();
+        myScore.score = 0;
+        // myGameArea.start();
+        if(highscore > 0){
+            myHighScore.text = "High Score: " + highscore;
+         }else{
+             myHighScore.text = ""
+         }
+        for(var i = 0; i < 3; i+=1){
+            lives.push(new component(30, 30, "blue", i*40, 10, "life"))
+        }
+    } 
 }
 var ctx = myGameArea.canvas
 ctx.addEventListener("mousemove", setMousePosition, false);
+ctx.addEventListener("mousedown", playAgain, false);
+
+
 ctx.style.cursor = 'none';
 function setMousePosition(e) {
  mouseX = e.clientX;
@@ -66,6 +96,13 @@ function component(width, height, color, x, y, type) {
             ctx.drawImage(face, this.x, this.y, width, height);
             // ctx.fillStyle = color;
             // ctx.fillRect(this.x, this.y, this.width, this.height);
+        }else if(this.type == "again"){
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+            ctx.font = "30px Consolas"
+            ctx.fillStyle = "white";
+            ctx.fillText("Play Again", this.x+10, this.y+40);
+            myGameArea.canvas.style.cursor = 'crosshair';
         }else{
             ctx.drawImage(pod, this.x, this.y, width, height);
         }
@@ -105,44 +142,58 @@ function component(width, height, color, x, y, type) {
 }
 
 function updateGameArea() {
+    if(ended){
+        myGameArea.clear();
+        endGame();
+        return null;
+    }
     var x, height, gap, minHeight, maxHeight, minGap, maxGap;
     for (i = 0; i < myObstacles.length; i += 1) {
         if (myGamePiece.crashWith(myObstacles[i])) {
             myScore.score += 1;
             myObstacles.splice(i, 1);
             i -= 1;
-        }else if(myObstacles[i].y >= ctx.height){
+        }else if(myObstacles[i].y >= myGameArea.canvas.height){
             myObstacles.splice(i, 1);
             i -= 1;
+            var life = lives.pop();
+            if(typeof life == 'undefined'){
+                endGame();
+                ended = true;
+            }
         }
 
     }
     myGameArea.clear();
     myGameArea.frameNo += 1;
-    if (myGameArea.frameNo == 1 || everyinterval(100)) {
+    if (myGameArea.frameNo == 1 || everyinterval(70)) {
         x = myGameArea.canvas.width;
-        width = 50;
-        height = 50;
-        x = Math.random()*myGameArea.canvas.width
+        width = 60;
+        height = 60;
+        x = Math.random()*(myGameArea.canvas.width-width);
         var obstacle = new component(width, height, "red", x, 0);
         obstacle.gravity = 0.05; 
         myObstacles.push(obstacle);
     }
-    if(everyinterval(500)){
+    if(everyinterval(350)){
         if(fallSpeed < 40){
-            fallSpeed *= 1.2;
+            fallSpeed *= 1.3;
         }
     }
     for (i = 0; i < myObstacles.length; i += 1) {
         myObstacles[i].update();
         myObstacles[i].fall();
     }
+    for(i = 0; i < lives.length; i += 1){
+        lives[i].update();
+    }
     myScore.text="SCORE: " + myScore.score;
     myScore.update();
-    mySpeed.text = "SPEED: " + Math.floor(fallSpeed);
-    mySpeed.update();
+    // mySpeed.text = "SPEED: " + Math.floor(fallSpeed);
+    myHighScore.update();
     myGamePiece.update();
     myGamePiece.newPos();
+    
     
 }
 
@@ -153,4 +204,32 @@ function everyinterval(n) {
 
 function accelerate(n) {
     myGamePiece.gravity = n;
+}
+
+function endGame(){
+    gameover = new component("30px", "Consolas", "black", 120, myGameArea.canvas.height/2, "text");
+    score = new component("30px", "Consolas", "black", 120, myGameArea.canvas.height/2+30, "text");
+    gameover.text = "Game Over";
+    score.text = "Score: "+ myScore.score;
+    
+    if(myScore.score >= highscore){
+        high = new component("30px", "Consolas", "black", 120, myGameArea.canvas.height/2+60, "text");
+        highscore = myScore.score;
+        high.text = "New Highscore!"
+        high.update();
+    }
+    gameover.update();
+    score.update();
+    myPlayAgain = new component(200, 60, "green", 120, myGameArea.canvas.height/2+90, "again");
+    myPlayAgain.update();
+    
+    // clearInterval(myGameArea.interval);
+    // return null;
+} 
+
+function playAgain(){
+    if(ended){
+        console.log("again");
+        myGameArea.again();
+    }
 }
